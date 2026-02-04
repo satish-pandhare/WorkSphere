584a52017637161fbe470654f8a19752852f37db
satish-pandhare <157160579+satish-pandhare@users.noreply.github.com>
2026-02-05 00:59:08 +0530

    Add CORS origin for production URL

--- server/utils/socket.js
+++ server/utils/socket.js
@@ -9,7 +9,8 @@ let io
 export function initSocket(server) {
   io = new Server(server, {
     cors: {
-      origin: ['http://localhost:5173'],
+      origin: ['http://localhost:5173',
+              'https://work-sphere-alpha.vercel.app'],
       credentials: true,
     },
   })
