38f5317098d4f52da0803df7b1f2791b2c5e8120
satish-pandhare <157160579+satish-pandhare@users.noreply.github.com>
2026-02-05 00:50:20 +0530

    Update API and image URLs for production

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API_URL = 'https://worksphere-dq95.onrender.com/api/v1';
-export const IMAGE_URL = 'https://worksphere-dq95.onrender.com/';
+export const API_URL = 'https://worksphere-ft13.onrender.com/api/v1';
+export const IMAGE_URL = 'https://worksphere-ft13.onrender.com/';
 //
 // export const API_URL = 'http://localhost:3000/api/v1';
 // export const IMAGE_URL = `http://localhost:3000/`;
